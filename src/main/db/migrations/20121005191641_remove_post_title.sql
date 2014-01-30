33b8002ea3665eb72bf46899e756f08336b4a347
Walden Raines <walden@subrosagames.com>
2014-01-30 09:10:54 -0500

    Using : instead of new lines for post title/content db migration.

--- src/main/db/migrations/20121005191641_remove_post_title.sql
+++ src/main/db/migrations/20121005191641_remove_post_title.sql
@@ -1,3 +1,3 @@
 ALTER TABLE post RENAME COLUMN body to content;
-UPDATE post SET content = title || E'\r\r' || content;
+UPDATE post SET content = title || ': ' || content;
 ALTER TABLE post DROP COLUMN title;
