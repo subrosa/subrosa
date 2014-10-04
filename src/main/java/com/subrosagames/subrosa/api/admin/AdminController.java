a3df87fecb5bd0c4858dfe4994758021cf9afbe2
Josiah Gore <josiahgore@gmail.com>
2014-10-04 11:49:47 -0400

    exposes configurations via admin endpoint and updates ec2 config

--- src/main/java/com/subrosagames/subrosa/api/admin/AdminController.java
+++ src/main/java/com/subrosagames/subrosa/api/admin/AdminController.java
@@ -1,8 +1,13 @@
 package com.subrosagames.subrosa.api.admin;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.subrosagames.subrosa.mgmt.config.ExposablePropertyPlaceholderConfigurer;
 
 /**
  * Exposes administrative functionality.
@@ -11,22 +16,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
+    private ExposablePropertyPlaceholderConfigurer placeholderConfigurer;
+
     /**
      * Dumps the application configuration.
+     *
      * @return application configuration dump
      */
     @RequestMapping("/configuration")
     @ResponseBody
     public String configuration() {
         StringBuilder sb = new StringBuilder();
-        /*sb.append("Build Properties:\n\n");
-        sb.append(buildProperties.getAllAsString()).append("\n\n");
-        sb.append("Configuration Properties:\n\n");
-        sb.append(configProperties.getAllAsString()).append("\n\n");*/
+        Map<String, String> resolvedProperties = placeholderConfigurer.getResolvedProperties();
+
         PropertiesFormatter systemFormatter = new PropertiesFormatter(System.getProperties());
         sb.append("System Properties:\n\n");
         sb.append(systemFormatter.getAllAsString()).append("\n\n");
+        sb.append("Configuration Properties:\n\n");
+        for (Map.Entry<String, String> entry : resolvedProperties.entrySet()) {
+            sb.append(entry.getKey()).append(" = ").append(entry.getValue()).append("\n");
+        }
+        sb.append("\n\n");
+
         return sb.toString();
     }
 
 }
+
