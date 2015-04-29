c36bb2e9b9826340c1dd7c059efab09dd93dcd96
Josiah Gore <josiahgore@gmail.com>
2015-04-29 16:28:04 -0400

    install java8 on the vagrant box and modify the sr script to run inside the VM

--- script/vagrant/provision.sh
+++ script/vagrant/provision.sh
@@ -5,10 +5,19 @@ uname -r
 
 wget -q -O - https://get.docker.io/gpg | apt-key add -
 echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
+
 apt-get update -qq
 apt-get install -q -y linux-image-generic-lts-trusty
 apt-get install -q -y linux-headers-$(uname -r)
 apt-get install -q -y build-essential dkms
+
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \
+  add-apt-repository -y ppa:webupd8team/java && \
+  apt-get update && \
+  apt-get install -y oracle-java8-installer && \
+  rm -rf /var/lib/apt/lists/* && \
+  rm -rf /var/cache/oracle-jdk8-installer
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 
 curl -s -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
